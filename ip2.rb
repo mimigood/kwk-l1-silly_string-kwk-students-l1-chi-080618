2f4847e5ca1a2264896d0d5fd82021cd91efd820
mimigood <mbgss07@gmail.com>
2018-08-06 20:22:13 +0000

    automatically backed up by learn

---
+++ ip2.rb
@@ -0,0 +1,11 @@
+fav_animal = "monkeys"
+fav_place = "Chicago"
+fav_artist = "Chance"
+
+def my_fav_things
+  puts "I love #{fav_animal}!"
+  puts "I love #{fav_place}!"
+  puts "I Love #{fav_artist}"
+end
+
+my_fav_things
